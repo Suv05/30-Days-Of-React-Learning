1b02cc62da855477eb2a5f400109424f60c30cd3
suvam <suvampravu@gmail.com>
2024-03-02 05:47:35 +0530

    Day-8 of react Learning

---
+++ 23-Passing-Children/src/App.jsx
@@ -0,0 +1,23 @@
+import Container from "./Components/Container";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <h1>Hello i am suv</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nostrum
+          ab eveniet quo molestias quae facere pariatur nobis nihil maiores!
+        </p>
+      </Container>
+      <Container>
+        <h1>Hello i am Raj</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nostrum
+          ab eveniet quo molestias quae facere pariatur nobis nihil maiores!
+        </p>
+      </Container>
+    </>
+  );
+}
+export default App;
