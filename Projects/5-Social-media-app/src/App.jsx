d6cce7e7e1a1e9e8abfc669a9ead973a8c66e4b1
suvam <suvampravu@gmail.com>
2024-03-23 21:54:42 +0530

    Day-20 of react

--- Projects/5-Social-media-app/src/App.jsx
+++ Projects/5-Social-media-app/src/App.jsx
@@ -4,14 +4,16 @@ import Postlist from "./components/Postlist";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { useState } from "react";
 
 function App() {
+  const [selectTab, setSelectTab] = useState("Create Post");
+
   return (
     <div className="appContainer">
       <Header></Header>
-      <Sidebar></Sidebar>
-      <CreatePost />
-      <Postlist />
+      <Sidebar selectTab={selectTab} setSelectTab={setSelectTab}></Sidebar>
+      {selectTab === "Home" ? <Postlist /> : <CreatePost />}
       <Footer />
     </div>
   );
