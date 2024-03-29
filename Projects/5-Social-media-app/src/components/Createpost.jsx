0f8a9a3cefa1a5aa3fe81b31a70ee1cc42cdfafd
suvam <suvampravu@gmail.com>
2024-03-29 18:40:59 +0530

    day 23 of react

--- Projects/5-Social-media-app/src/components/Createpost.jsx
+++ Projects/5-Social-media-app/src/components/Createpost.jsx
@@ -1,28 +1,74 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function CreatePost({}) {
+  useRef()
+  useRef()
+  useRef()
+  useRef()
+  useRef()
+  useRef()
   return (
-    <div>
+    <div className="createPost">
+      {/* Title */}
       <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          Email address
+        <label htmlFor="title" className="form-label">
+          <i>Title</i>
         </label>
         <input
-          type="email"
+          type="text"
           className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
+          id="title"
+          placeholder="Give a crunchy title to ur post 😘"
         />
       </div>
+      {/* post body */}
       <div className="mb-3">
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">
-          Example textarea
+        <label htmlFor="body" className="form-label">
+          <i>Post</i>
         </label>
         <textarea
           className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
+          placeholder="Post your secreate 🤫"
+          id="body"
+          rows="5"
         ></textarea>
+      </div>
+
+      {/* reaction */}
+      <div className="mb-3">
+        <label htmlFor="reaction" className="form-label">
+          <i>Reaction</i>
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="reaction"
+          placeholder="Give others react to ur post 🤓"
+        />
+      </div>
+      {/* userid */}
+      <div className="mb-3">
+        <label htmlFor="userid" className="form-label">
+          <i>User-Id</i>
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="userid"
+          placeholder="Give a sexy User-Id 🫦"
+        />
+      </div>
+      {/* tags */}
+      <div className="mb-3">
+        <label htmlFor="tag" className="form-label">
+          <i>Tags</i>
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="tag"
+          placeholder="Give a asethetic tag in ur post 😶‍🌫️"
+        />
       </div>
     </div>
   );
