0fe6662238217ff7d66e11b05677f56408b943da
suvam <suvampravu@gmail.com>
2024-03-29 18:41:24 +0530

    day 23 of react

--- Projects/5-Social-media-app/src/components/Post.jsx
+++ Projects/5-Social-media-app/src/components/Post.jsx
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useContext } from "react";
+import { BiSolidLike } from "react-icons/bi";
+import { FaCommentAlt } from "react-icons/fa";
+import { FaShare } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { PostContext } from "../contexts/PostlistsProvide";
 
-function Post({}) {
+/////////////////////==============///////////////////////
+
+function Post({ id, title, body, tags }) {
+  //calling function for delete post
+  const { removePost } = useContext(PostContext);
+
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src="..." className="card-img-top" alt="..." />
+    <div
+      className="card cardPose col-lg-6 mx-3 my-3 ml"
+      style={{ width: "23rem", height: "300px" }}
+    >
+      {/* You can use the post data to fill in the card */}
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <h2
+          className="card-title"
+          style={{
+            fontSize: "1rem",
+            fontWeight: "bolder",
+            fontVariant: "common-ligatures",
+          }}
+        >
+          {title.toUpperCase()}
+          <span className="delete">
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => {
+                removePost(id);
+              }}
+            >
+              <MdDelete />
+            </button>
+          </span>
+        </h2>
+        <p className="card-text" style={{ fontSize: "larger" }}>
+          {body}
         </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <ul style={{ listStyle: "none" }}>
+          {/* Assuming tags is an array */}
+          {tags.map((tag, index) => (
+            <li
+              key={index}
+              style={{ display: "inline-block", marginRight: "5px" }}
+            >
+              <span className="badge text-bg-info m-2">
+                #{tag.toUpperCase()}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <button type="button" className="btn btn-outline-warning m-3">
+          <BiSolidLike /> 99
+        </button>
+        <button type="button" className="btn btn-outline-success m-3">
+          <FaCommentAlt /> 13
+        </button>
+        <button type="button" className="btn btn-outline-info m-3">
+          <FaShare /> 11
+        </button>
       </div>
     </div>
   );
