44269625117d109232d47daab9b29700d7d0e7a4
suvam <suvampravu@gmail.com>
2024-04-03 19:08:27 +0530

    Day-24 in react

--- Projects/5-Social-media-app/src/components/Postlist.jsx
+++ Projects/5-Social-media-app/src/components/Postlist.jsx
@@ -1,12 +1,23 @@
 import React, { useContext } from "react";
 import Post from "./Post";
+import Welcome from "./Welcome";
 import { PostContext } from "../contexts/PostlistsProvide";
 
 function Postlist() {
   // using context to fetch posts from Postlist
-  const { posts } = useContext(PostContext);
+  const { posts, showPost } = useContext(PostContext);
+
+  //Define the getPost method
+  const handelGetPost = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((res) => {
+        showPost(res.posts);
+      });
+  };
   return (
     <>
+      {posts.length === 0 && <Welcome getPost={handelGetPost} />}
       <div className="row">
         {posts.map((post) => (
           <Post key={post.id} {...post} /> // assuming you pass all props to Post component
