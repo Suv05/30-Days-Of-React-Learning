7f046309eaf465fbce60309d12abbeaa7eb5f69f
suvam <suvampravu@gmail.com>
2024-04-03 19:08:08 +0530

    Day-24 in react

--- Projects/5-Social-media-app/src/components/Header.jsx
+++ Projects/5-Social-media-app/src/components/Header.jsx
@@ -4,7 +4,8 @@ function Header(){
     <div className="container">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlinkHref="#bootstrap"></use></svg>
+          <img src="./public/s.svg" alt="" width="55"
+        height="45"/>
         </a>
 
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
