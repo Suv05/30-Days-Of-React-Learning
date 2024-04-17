b56c58347434998c4eaae15e2bb9d6ef97a27150
suvam <suvampravu@gmail.com>
2024-04-18 00:19:54 +0530

    Day-26 in react

--- 32-Custom-Hooks/src/App.jsx
+++ 32-Custom-Hooks/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import useFetch from "./components/Fetch";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { data, loading, error } = useFetch(
+    "https://v2.jokeapi.dev/joke/Any?"
+  );
+  if (loading) {
+    return <div>Loading.....</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Category:{data.category}</h1>
+        <p>type:{data.type}😂</p>
+        <p>me:{data.setup}</p>
+        <p>friend:{data.delivery}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
