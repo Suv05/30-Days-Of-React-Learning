f23b3d606e7579c2dbd2ecf71ce5e51fb2e8a426
suvam <suvampravu@gmail.com>
2024-04-30 22:50:05 +0530

    day-27 in react

--- 34-Redux-Tool-Kit/2-react-with-redux/src/App.jsx
+++ 34-Redux-Tool-Kit/2-react-with-redux/src/App.jsx
@@ -1,16 +1,25 @@
-import './App.css'
-import Header from './components/Header'
-import Content from './components/Content'
+import "./App.css";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Render from "./components/Render";
 
 function App() {
-  
-
   return (
     <>
-     <Header/> 
-     <Content></Content>
+      <Header />
+      <Content />
+      {/* <Render /> */}
+      <center>
+        <button
+          type="button"
+          className="btn btn-outline-success btn-lg gap-3"
+          fdprocessedid="nbygdm9"
+        >
+          <i>WANT TO SEE JOKER 🃏</i>
+        </button>
+      </center>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
